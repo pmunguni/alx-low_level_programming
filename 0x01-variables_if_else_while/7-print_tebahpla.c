0faf5b6afb5034b1de5b9f388af00be16bcfcb12
Patricia Munguni <patriciamunguni05@gmail.com>
2023-07-25 07:40:21 -0700

    print

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -3,13 +3,13 @@
 /**
  * main - print a..z in reverse
  *
- * Return : give back 0 (success)
+ * Return: give back 0 (success)
  */
 int main(void)
 {
 	int i;
 
-	for (i = 122; i <= 97; i--)
+	for (i = 122; i >= 97; i--)
 	{
 		putchar(i);
 	}
