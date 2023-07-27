28c60fbc976db6c726d1f76f0131e618826db1c8
Patricia Munguni <patriciamunguni05@gmail.com>
2023-07-27 03:18:07 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet- prints the alphabet in lower case
+ *
+ * Return: it does not give back anything
+ */
+void print_alphabet(void)
+{
+	char alph[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		_putchar(alph[i]);
+		_putchar('\n');
+	}
+}
