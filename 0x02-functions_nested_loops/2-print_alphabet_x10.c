f473a0bef9d7732369c8ed3d69483b996d75c0d7
Patricia Munguni <patriciamunguni05@gmail.com>
2023-07-27 04:04:50 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet in lower case 0 times
+ *@letter
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i <= 10; i++)
+	{
+		char letter;
+
+		for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+	}
+	_putchar('\n');
+}
