81ee5824dc5e162f0173b07e860e7b95e193f8bf
Patricia Munguni <patriciamunguni05@gmail.com>
2023-07-27 06:32:19 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isalpha -checks for alphabetic character
+ * @c: character to e checked
+ *
+ * Returns: give 1 if ttrue else 0
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' || c >= 'A') && (c <= 'z' || c <= 'Z'))
+		return (1);
+	else
+		return (1);
+}
