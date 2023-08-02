a8015f0ffdf92939f132c7a72b9d154ec9967b37
Patricia Munguni <patriciamunguni05@gmail.com>
2023-08-02 06:10:44 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *_isupper - cheks for uppercase letters
+ *@c: character to be cheked
+ *
+ * Return: give 0 (success)
+ */
+int _isupper(int c)
+{
+	int i;
+
+	for (i == 'A'; i <= 'Z'; i++)
+	{
+		if (c == i)
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+	}
+}
