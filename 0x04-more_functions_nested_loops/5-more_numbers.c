69761497e0a234a7fd7e0f1cbfc5f6efb57be9a1
Patricia Munguni <patriciamunguni05@gmail.com>
2023-08-02 07:48:03 -0700

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times numbers,0 to 14, followed by a new line.
+ */
+void more_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+}
+
