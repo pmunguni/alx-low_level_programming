c4a372c85545ac6f7ba60a847e40cf0ac1b2c29c
Patricia Munguni <patriciamunguni05@gmail.com>
2023-08-05 16:36:44 -0700

    prime

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * largestfactor - prints the the largest factor
+ * @number: the number to be cheked
+ *
+ * Return; give bak the largest prime factor
+ */
+long largestfactor(long number)
+{
+	long largestfactor = 2;
+
+	while (number >= largestfactor)
+	{
+		if (number < 0)
+			printf("please give positive number");
+		if (number % largestfactor == 0)
+		{
+			number /= largestfactor;
+		}
+		else
+		{
+			largestfactor++;
+		}
+	}
+	return (largestfactor);
+}
+/**
+ * main - give largest prime factor
+ *
+ * Return: give back 0 for success
+ */
+int main(void)
+{
+	long number = 612852475143;
+	long largestprime = largestfactor(number);
+
+	printf("largest prime factor is %li \n", largestprime);
+	return (0);
+}
