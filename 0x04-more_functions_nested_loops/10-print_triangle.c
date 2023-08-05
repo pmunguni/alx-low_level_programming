b36f5199e466342edeeb90b180d773addc10921d
Patricia Munguni <patriciamunguni05@gmail.com>
2023-08-05 14:59:51 -0700

    triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -6,19 +6,19 @@
  */
 void print_triangle(int size)
 {
-	int i, j;
+	int i, j, k;
 
-	if (size == 0 || size <= 0)
+	if (size <= 0)
 	{
 		_putchar('\n');
 	}
 	for (i = 1; i <= size; i++)
 	{
-		for (j = i; j < 1; j--)
+		for (j = size - i; j >= 1; j--)
 		{
 			_putchar(' ');
 		}
-		for (k = 1; k < i; k++)
+		for (k = 1; k <= i; k++)
 		_putchar('#');
 	}
 	_putchar('\n');
