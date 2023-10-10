d554af51f959948c12c9cae35e94c952693745e1
Patricia <patriciamunguni05@gmail.com>
2023-10-10 06:45:24 -0700

    revers

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * rev_string - Reverses a string in place.
+ * @s: The string to be reversed.
+ */
+void rev_string(char *s)
+{
+    char *end = s;
+
+    // Find the end of the string
+    while (*end != '\0')
+    {
+        end++;
+    }
+
+    // Print the reversed string
+    while (end > s)
+    {
+        end--;
+        putchar(*end);
+    }
+    putchar('\n');
+}
